bc1faf786668a400bcf35e85a2236e04429d2737
Andy Fleener <andrew.fleener@sportngin.com>
2014-04-02 14:17:28 -0500

    Add README and metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'opsworks-migrations'
+maintainer       'Sport Ngin'
+maintainer_email 'infrasturcture@sportngin.com'
+license          'All rights reserved'
+description      'Run Rails Migrations on Demand for AWS OpsWorks'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "deploy"
+
