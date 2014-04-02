4a801fd4a52d5ebf4478ad4637d4d63dccd726a4
Andy Fleener <andrew.fleener@sportngin.com>
2014-04-02 16:23:57 -0500

    update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
 name             'opsworks-migrations'
 maintainer       'Sport Ngin'
 maintainer_email 'infrasturcture@sportngin.com'
-license          'All rights reserved'
+license     "MIT"
+version     "0.0.1"
 description      'Run Rails Migrations on Demand for AWS OpsWorks'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
