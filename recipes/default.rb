84241a38ed8865c564aa079ce0c7f2eb19753ffd
Andy Fleener <andrew.fleener@sportngin.com>
2014-04-02 16:01:44 -0500

    Add a default recipe

---
+++ recipes/default.rb
@@ -0,0 +1,29 @@
+include_recipe "deploy"
+
+node[:deploy].each do |application, deploy|
+  node.override[:deploy][application][:deploy_to] = "#{deploy[:deploy_to]}/#{deploy[:migrations_dir] || "migrations"}"
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+
+  ruby_block "Run Bundle Install" do
+    block do
+      Chef::Log.info(`cd #{deploy[:deploy_to]}/current &&  bundle install --path #{deploy[:deploy_to]}/shared/bundle --without=test development --deployment`)
+      raise "Bundle Install FAILED" unless $?.success?
+    end
+  end
+
+  ruby_block "Running Migrations" do
+    block do
+      Chef::Log.info(`cd #{deploy[:deploy_to]}/current &&  RAILS_ENV=#{deploy[:rails_env]} #{deploy[:migration_command]}`)
+      raise "Migrations FAILED" unless $?.success?
+    end
+  end
+end
