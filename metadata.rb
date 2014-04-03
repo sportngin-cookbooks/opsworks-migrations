3b18ff547d1ac8c3009e5b7be76cc89d32ed9353
Andy Fleener <andrew.fleener@sportngin.com>
2014-04-03 13:37:51 -0500

    Ugh fix metadata.rb load problem

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'infrasturcture@sportngin.com'
 license     "MIT"
 version     "0.0.1"
 description      'Run Rails Migrations on Demand for AWS OpsWorks'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
 version          '0.1.0'
 
 depends "deploy"
